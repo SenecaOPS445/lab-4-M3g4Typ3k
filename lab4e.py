5cf10ab68c7e412abd61fa8e972fc2126107888f
Jakob Tuma <jtuma@myseneca.ca>
2025-02-08 23:18:04 -0500

    Added Complete lab4e

--- lab4e.py
+++ lab4e.py
@@ -4,9 +4,9 @@
 def is_digits(sobj):
     # place code here - loop through each character in sobj
      for char in sobj:
-      if sobj in '0123456789':
-       return True
-     return False
+      if not ('0' <= char <= '9'):
+       return False
+     return True
 if __name__ == '__main__':
     test_list = ['x3058','3058','8503x','8503']
     for item in test_list:
